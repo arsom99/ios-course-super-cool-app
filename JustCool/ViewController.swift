2952bbb495910c11e69f09efef0c43f62a245c45
Aristotle Som <aristotlesom@imac-toutes-aristotle.local>
2016-04-28 10:27:30 +0300

    Initial Commit

---
+++ JustCool/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JustCool
+//
+//  Created by Aristotle Som on 28/4/16.
+//  Copyright © 2016 Aristotle Som. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
